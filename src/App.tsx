69a94f41bc5381502396f41cc915661588f8a7b9
Avila-Pato <p.avilaf1998@gmail.com>
2024-07-23 22:35:01 -0400

    Aprendiendo firebase con rect y ts

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+
+      <h1>App</h1>
+      <Button> Ejemplo buton</Button>
+    </div>
+    
+  )
+}
+
+export default App
